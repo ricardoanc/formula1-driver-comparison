289d6e20919cfe5852d5ce860c2ebb6c8b90fbd4
ricardoanc <ricardoncd@gmail.com>
2025-08-01 00:49:30 -0400

    Introducing Pole positions

--- app.py
+++ app.py
@@ -31,12 +31,17 @@ driver2 = st.selectbox(
 # year, driver1, driver2 = 2022, 'VER', 'ALO'
 
 winners = functions.get_race_winners(year)
+pole_winners = functions.get_pole_winners(year)
 
 driver1_wins = functions.count_wins(driver1, winners)
 driver2_wins = functions.count_wins(driver2, winners)
 
+driver1_poles = functions.count_wins(driver1, pole_winners)
+driver2_poles = functions.count_wins(driver2, pole_winners)
+
 # print(f'{driver1} wins in {year}: {driver1_wins}')
 # print(f'{driver2} wins in {year}: {driver2_wins}')
 
-st.write(f'{driver1} wins in {year}: {driver1_wins}')
-st.write(f'{driver2} wins in {year}: {driver2_wins}')
+st.write(f'{driver1} had {driver1_wins} wins in season {year}./n While {driver2} had {driver2_wins} wins.')
+st.write(f'{driver1} had {driver1_poles} pole positions in season {year}./n While {driver2} had {driver2_poles} wins.')
+
--- functions.py
+++ functions.py
@@ -19,6 +19,21 @@ def get_race_winners(year):
         df = pd.concat([df, pd.DataFrame([{'Circuit': circuit, 'Winner': winner_code}])], ignore_index=True)
     return df
 
+
+# Returns a list of the winner of each pole position given the season
+def get_pole_winners(year):
+    schedule = ergast.get_race_schedule(season=year)
+    
+    df = pd.DataFrame(columns=['Circuit', 'Winner'])
+
+    for i, round_number in enumerate(schedule['round'], start=1):
+        pole_winner = ergast.get_driver_info(season=year, round=round_number, grid_position=1)['driverCode'].iloc[0]
+        circuit = schedule[schedule['round'] == round_number]['circuitId'].iloc[0]
+
+        df = pd.concat([df, pd.DataFrame([{'Circuit': circuit, 'Winner': pole_winner}])], ignore_index=True)
+    return df
+
+
 # Returns total count of wins per racer
 def count_wins(racer, winners):
     total_wins = (winners['Winner'] == racer).sum()
