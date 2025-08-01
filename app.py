71fe519a68d1458de7b82fcbbd30b86ce8c13f33
ricardoanc <ricardoncd@gmail.com>
2025-08-01 18:08:35 -0400

    Showing all drivers per year

--- app.py
+++ app.py
@@ -5,32 +5,38 @@ import time
 
 ff1.Cache.enable_cache('./cache')
 
+years = ['Select...'] + [str(y) for y in range(1950, 2025)]
+
 year = st.selectbox(
     'Select the racing season:',
-    ('Select...','2024','2023','2022','2021','2020','2019','2018','2017')
+    years
 )
 
 st.write(f'SEASON {year}')
 
-# drivers = functions.get_drivers
+
+drivers = functions.get_drivers(year)
+
 
 driver1 = st.selectbox(
     'Select Driver 1:',
-    ('Select...','VER','PER','SAI','LEC','RUS','ALO')
+    drivers
 )
 
 driver2 = st.selectbox(
     'Select Driver 2:',
-    ('Select...','VER','PER','SAI','LEC','RUS','ALO')
+    drivers
 )
 
-# year, driver1, driver2 = 2022, 'VER', 'ALO'
+# year, driver1, driver2 = 2024, 'VER', 'ALO'
 
 if year != 'Select...' and driver1 != 'Select...' and driver2 != 'Select...':
     st.write(f"SEASON {year}: Comparing {driver1} vs {driver2}")
 
     winners = functions.get_race_winners(year)
+    time.sleep(0.5)
     pole_winners = functions.get_pole_winners(year)
+    time.sleep(0.5)
 
     driver1_wins = functions.count_wins(driver1, winners)
     driver2_wins = functions.count_wins(driver2, winners)
@@ -45,4 +51,4 @@ if year != 'Select...' and driver1 != 'Select...' and driver2 != 'Select...':
     st.write(f'{driver1} pole positions: {driver1_poles}.\n{driver2} pole positions: {driver2_poles}.')
 
 else:
-    print('Please select all values.')
+    st.write('Please select all values.')
--- functions.py
+++ functions.py
@@ -8,8 +8,25 @@ ergast = Ergast()
 
 # Returns a list of drivers on a given season
 def get_drivers(year):
-    season_drivers = ergast.get_driver_info(season=year)['driverCode'].drop_duplicates()
+    season_drivers = ergast.get_driver_info(season=year)['driverId'].drop_duplicates()
     return season_drivers
+
+
+# Just in case: get the list of all drivers from 1950 to 2024
+# def jic_full_drivers_list():
+#     drivers_list = []
+
+#     for year in range(1950,2025):
+#         season_drivers = get_drivers(year)
+#         time.sleep(0.5)
+#         for driver in season_drivers:
+#             drivers_list.append({'Driver Name': driver, 'Season': year})
+
+#     all_time_drivers = pd.DataFrame(drivers_list)
+
+#     all_time_drivers.to_csv('./drivers_list.csv', index=False, encoding='utf-8')
+    
+#     return
 
 
 # Returns a list of the winner of each race given the season
@@ -19,7 +36,7 @@ def get_race_winners(year):
     df = pd.DataFrame(columns=['Circuit', 'Winner'])
 
     for i, round_number in enumerate(schedule['round'], start=1):
-        winner_code = ergast.get_driver_info(season=year, round=round_number, results_position=1)['driverCode'].iloc[0]
+        winner_code = ergast.get_driver_info(season=year, round=round_number, results_position=1)['driverId'].iloc[0]
         circuit = schedule[schedule['round'] == round_number]['circuitId'].iloc[0]
 
         df = pd.concat([df, pd.DataFrame([{'Circuit': circuit, 'Winner': winner_code}])], ignore_index=True)
@@ -33,7 +50,7 @@ def get_pole_winners(year):
     df = pd.DataFrame(columns=['Circuit', 'Winner'])
 
     for i, round_number in enumerate(schedule['round'], start=1):
-        pole_winner = ergast.get_driver_info(season=year, round=round_number, grid_position=1)['driverCode'].iloc[0]
+        pole_winner = ergast.get_driver_info(season=year, round=round_number, grid_position=1)['driverId'].iloc[0]
         circuit = schedule[schedule['round'] == round_number]['circuitId'].iloc[0]
 
         df = pd.concat([df, pd.DataFrame([{'Circuit': circuit, 'Winner': pole_winner}])], ignore_index=True)
