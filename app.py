79eba38758b2d13376a2b44fa69edeb266583363
ricardoanc <ricardoncd@gmail.com>
2025-08-01 01:08:58 -0400

    deleted buffer and added conditions to start

--- app.py
+++ app.py
@@ -7,31 +7,26 @@ ff1.Cache.enable_cache('./cache')
 
 year = st.selectbox(
     'Select the racing season:',
-    ('2024',
-     '2023',
-     '2022',
-     '2021',
-     '2020',
-     '2019',
-     '2018',
-     '2017')
+    ('Select...','2024','2023','2022','2021','2020','2019','2018','2017')
 )
 
 st.write(f'SEASON {year}')
 
 driver1 = st.selectbox(
     'Select Driver 1:',
-    ('VER','PER','SAI','LEC','RUS','ALO'),
+    ('Select...','VER','PER','SAI','LEC','RUS','ALO'),
 )
 
 driver2 = st.selectbox(
     'Select Driver 2:',
-    ('VER','PER','SAI','LEC','RUS','ALO'),
+    ('Select...','VER','PER','SAI','LEC','RUS','ALO'),
 )
 
 # year, driver1, driver2 = 2022, 'VER', 'ALO'
 
-if driver1 and driver2:
+if year != 'Select...' and driver1 != 'Select...' and driver2 != 'Select...':
+    st.write(f"SEASON {year}: Comparing {driver1} vs {driver2}")
+
     winners = functions.get_race_winners(year)
     time.sleep(0.5)
     pole_winners = functions.get_pole_winners(year)
@@ -43,9 +38,11 @@ if driver1 and driver2:
     driver1_poles = functions.count_wins(driver1, pole_winners)
     driver2_poles = functions.count_wins(driver2, pole_winners)
 
-# print(f'{driver1} had {driver1_wins} wins in season {year}.\n While {driver2} had {driver2_wins} wins.\n\n')
-# print(f'{driver1} had {driver1_poles} pole positions in season {year}.\n While {driver2} had {driver2_poles} pole positions.')
+    # print(f'{driver1} had {driver1_wins} wins in season {year}.\n While {driver2} had {driver2_wins} wins.\n\n')
+    # print(f'{driver1} had {driver1_poles} pole positions in season {year}.\n While {driver2} had {driver2_poles} pole positions.')
 
-st.write(f'{driver1} had {driver1_wins} wins in season {year}.\n While {driver2} had {driver2_wins} wins.\n\n')
-st.write(f'{driver1} had {driver1_poles} pole positions in season {year}.\n While {driver2} had {driver2_poles} pole positions.')
+    st.write(f'{year}\n{driver1} wins: {driver1_wins}.\n{driver2} wins: {driver2_wins}.\n\n')
+    st.write(f'{driver1} pole positions: {driver1_poles}.\n{driver2} pole positions: {driver2_poles}.')
 
+else:
+    pass
